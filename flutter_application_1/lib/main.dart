c7ce807a85a35d62a29f9f573a888da4fc77a054
ywkim727 <uddn@naver.com>
2024-04-19 09:58:27 +0900

    Study Text Widget

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,21 +10,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Hello, Flutter!'),
-        backgroundColor: Colors.redAccent,
+        appBar: AppBar(
+          title: const Text('Hello, Flutter!'),
+          backgroundColor: Colors.redAccent,
         ),
-        body: Center(
-          child: Text(
-            'Hello, World!',
-            style: TextStyle(
-              fontSize: 24, 
-              color: Colors.redAccent, 
-              fontWeight: FontWeight.bold
-            ), 
+        body: const Center(
+            child: Text.rich(
+              TextSpan(
+                style: TextStyle(
+                  fontSize: 20
+                ),
+                children: [
+                  TextSpan(
+                    text: 'Lorem Ipsum is ',
+                  ),
+                  TextSpan(
+                    text: 'simply',
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' dummy text',
+                  ),
+                ]
+              )
+            )
           )
         ),
-      )
-    );
+      );
   }
 }
-
