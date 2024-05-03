b7dbb4685899b0bbaf14047f2ce1e635855a85a2
ywkim727 <uddn@naver.com>
2024-05-03 10:47:40 +0900

    study Layout_instargram_02

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
             ),
             //top
             Container(
-              padding: EdgeInsets.fromLTRB(10, 6, 15, 0),
+              padding: const EdgeInsets.fromLTRB(10, 6, 15, 0),
               height: 50,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -54,7 +55,88 @@ class MyApp extends StatelessWidget {
               ),
             ),
             Expanded( //body
-              child: Row()
+              child: Column(
+                children: [
+                  //user list
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    height: 108,
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.grey.shade300,
+                          width: 0.5,
+                        )
+                      )
+                    ),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          //user
+                          User(userName: '내스토리',profilePicture: 'assets/Profile_picture_01.png',),
+                          User(userName: 'flutter_framework',profilePicture: 'assets/Profile_picture_02.png',),
+                          User(userName: 'soongsil_university',profilePicture: 'assets/Profile_picture_03.png',),
+                          User(userName: 'mobile_programming',profilePicture: 'assets/Profile_picture_04.png',),
+                          User(userName: 'global_media',profilePicture: 'assets/Profile_picture_05.png',),
+                          User(userName: 'awesome_application',profilePicture: 'assets/Profile_picture_06.png',),
+                        ],
+                      ),
+                    ),
+                  ),
+                  //feeds
+                  Column(
+                    children: [
+                      //userinfo
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        height: 52,
+                        child: Row(
+                          children: [
+                            Image.asset('assets/Profile_picture_02.png'),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            const Text(
+                              'flutter_framework',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const Spacer(),
+                            const Icon(
+                              Icons.more_horiz,
+                              size: 18,
+                            )
+                          ],
+                        ),
+                      ),
+                      SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            'assets/Feed_picture_01.png',
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                          Image.asset(
+                            'assets/Feed_picture_02.png',
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                          Image.asset(
+                            'assets/Feed_picture_03.png',
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                        ],
+                      ),
+                    )
+                    ],
+                    //Feed pictures
+                    
+                  )
+                ],
+              )
             ),
             Container(  //bottom
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
@@ -86,5 +168,78 @@ class MyApp extends StatelessWidget {
           
         ),
       );
+  }
+}
+
+class User extends StatelessWidget {
+  final String userName;
+  final String profilePicture;
+
+  const User({
+    super.key,
+    required this.userName,
+    required this.profilePicture,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+                          horizontal: 6
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 71,
+                                height: 71,
+                                padding: const EdgeInsets.all(2.5),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  gradient: LinearGradient(
+                                    begin:Alignment.bottomLeft,
+                                    end:Alignment.topRight,
+                                    stops:[0.2, 0.4, 0.6, 0.8, 1],
+                                    colors:[
+                                      Color(0xFFFFD600),
+                                      Color(0xFFFF7A00),
+                                      Color(0xFFFF0069),
+                                      Color(0xFFD300C5),
+                                      Color(0xFF7638FA),
+                                    ],
+                                  )
+                                ),
+                                child: Container(
+                                  padding: const EdgeInsets.all(2.5),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    shape: BoxShape.circle,
+                                  ),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                        image: AssetImage(
+                                          profilePicture
+                                        )
+                                      )
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 71,
+                                child: Text(
+                                  //my story
+                                  userName,
+                                  textAlign: TextAlign.center,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 11,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        );
   }
 }
